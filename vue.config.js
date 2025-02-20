04bbe7defd1afacb42c07ba471f5e6020939e2c8
Saymon Felipe <80544520+saymon-felipe@users.noreply.github.com>
2025-02-20 17:34:44 -0300

    atualização do vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,4 @@
-const fs = require('fs');
-const path = require('path');
-
-module.exports = {
-  transpileDependencies: true,
-  devServer: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, './localhost-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, './localhost-cert.pem')),
-    },
-    host: '0.0.0.0', // Permite acesso de qualquer dispositivo na rede
-    allowedHosts: 'all', // Permite qualquer host (dispositivo na rede local)
-    port: 8080,
-  },
-};
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
